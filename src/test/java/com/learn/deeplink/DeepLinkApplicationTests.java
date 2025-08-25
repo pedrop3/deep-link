1310d12d7dc5b479be1fda261240434509529b9d
Pedro Santos <pedro_henriquems321@hotmail.com>
2025-08-26 00:16:57 +0100

    init commit

---
+++ src/test/java/com/learn/deeplink/DeepLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.deeplink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
